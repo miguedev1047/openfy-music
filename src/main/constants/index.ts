3d2ff5e5649bad55845778cb7c03bdce3e96e3f4
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-12 12:56:11 -0500

    FEAT: add constants ⚒️

---
+++ src/main/constants/index.ts
@@ -0,0 +1,10 @@
+import { homedir } from 'os'
+import { join } from 'path'
+
+export const LOCAL_RESOURCE_PREFIX = 'local-resource://'
+export const EXTENSIONS = ['mp3', 'mp4']
+
+export const openfyMusicDir = join(homedir(), 'openfy-music')
+
+export const musicDir = join(openfyMusicDir, 'Music')
+export const cacheDir = join(openfyMusicDir, 'Cache')
