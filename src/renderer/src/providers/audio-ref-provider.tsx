3bf5378d8cb5fd6fce36cbdd02d0c67800e20470
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-12 13:24:12 -0500

    FEAT: add providers files ⚙️

---
+++ src/renderer/src/providers/audio-ref-provider.tsx
@@ -0,0 +1,25 @@
+import { createContext, use, useRef } from 'react'
+
+interface AudioRefContextProps {
+  audioRef: React.RefObject<HTMLAudioElement | null>
+}
+
+const AudioRefContext = createContext<AudioRefContextProps | null>(null)
+
+export const useAudioRef = () => {
+  const context = use(AudioRefContext)
+  if (!context) {
+    throw new Error('useAudioRef must be used within an AudioRefProvider')
+  }
+  return context
+}
+
+export function AudioRefProvider({ children }: { children: React.ReactNode }) {
+  const audioRef = useRef<HTMLAudioElement | null>(null)
+
+  return (
+    <AudioRefContext.Provider value={{ audioRef }}>
+      {children}
+    </AudioRefContext.Provider>
+  )
+}
