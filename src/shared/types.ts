55a015ed85ef88fdfe6fba73ee0362c0c0248f54
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-14 20:26:11 -0500

    CHORE: update types  ⚒️

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,6 +1,11 @@
-import { RemoveSongProps, SongProps } from "./models";
+import { PlaylistFolderProps, RenamePlaylistFolderProps, SongProps, UserConfig } from './models'
 
-export type GetSongs = () => Promise<SongProps[]>
+export type GetPlayLists = () => Promise<PlaylistFolderProps[]>
+export type GetSongsPlayList = (playlistName: string) => Promise<SongProps[]>
+export type GetConfigData = () => Promise<UserConfig>
+export type UpdateConfigData = (data: UserConfig) => Promise<void>
+export type RemovePlaylist = (playlistName: string) => Promise<void>
+export type RenamePlaylist = (data: RenamePlaylistFolderProps) => Promise<void>
+export type NewPlaylist = (playlistName: string) => Promise<void>
 export type ReadSong = (filename: string) => Promise<Buffer<ArrayBufferLike>>
-export type RemoveSong = (props: RemoveSongProps) => Promise<void>
-
+export type OpenFolder = (playlistName: string) => Promise<void>
