cd705132188849cb735022431f5c2776ac9cce10
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-15 16:32:03 -0500

    CHORE: refactor electron main and preload ⚙️

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,20 +4,40 @@ import { app, shell, BrowserWindow, ipcMain, protocol } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 
-import { handleCloseWindow, handleMinimizeWindow } from './lib/window-controls'
+import {
+  GetSongsPlayList,
+  NewPlaylist,
+  OpenFolder,
+  ReadSong,
+  RemovePlaylist,
+  RenamePlaylist,
+  UpdateConfigData
+} from '../shared/types'
+
+import {
+  handleCloseWindow,
+  handleMinimizeWindow,
+  toggleMaximizeWindow
+} from './lib/window-controls'
+import {
+  getPlayLists,
+  getSongsPlaylist,
+  newPlaylist,
+  removePlaylist,
+  renamePlaylist
+} from './lib/playlist-manager'
 import { openSongFolder } from './lib/open-song-folder'
-import { getSongList, readSong, removeSong } from './lib/song-manager'
-import { ReadSong, RemoveSong } from '../shared/types'
+import { readSong } from './lib/song-manager'
+
 import { localResource } from './helpers/local-resource'
-import { clearCacheMusicDir } from './helpers/music-dir'
+import { ensureDefaultPlaylistsFolders } from './utils/default-folder'
+import { ensureJsonConfig, getConfigData, updateConfigData } from './utils/config-data'
 
 const gotTheLock = app.requestSingleInstanceLock()
 
 const SIZES = {
   width: 1200,
   height: 840,
-  maxWidth: 1280,
-  maxHeight: 900,
   minWidth: 880,
   minHeight: 760
 }
@@ -34,6 +54,7 @@ function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     icon,
+    ...SIZES,
     title: 'Openfy Music',
     frame: false,
     center: true,
@@ -42,7 +63,6 @@ function createWindow(): void {
     roundedCorners: true,
     show: false,
     autoHideMenuBar: true,
-    ...SIZES,
     vibrancy: 'under-window',
     visualEffectState: 'active',
     titleBarStyle: 'hidden',
@@ -79,7 +99,6 @@ function createWindow(): void {
     app.on('second-instance', () => {
       if (mainWindow) {
         if (mainWindow.isMinimized()) mainWindow.restore()
-
         if (mainWindow.isFocused()) mainWindow.focus()
       }
     })
@@ -94,31 +113,63 @@ function createWindow(): void {
 
 protocol.registerSchemesAsPrivileged([{ scheme: 'local-resource', privileges: PRIVILEGES }])
 
+// ipcMain Functions //
+
 app.whenReady().then(() => {
+  protocol.handle('local-resource', (request) => localResource(request))
+
+  ipcMain.handle('play-list', () => getPlayLists())
+  ipcMain.handle('songs-playlist', (_event, ...args: Parameters<GetSongsPlayList>) =>
+    getSongsPlaylist(...args)
+  )
+
+  ipcMain.handle('get-config-data', () => getConfigData())
+  ipcMain.handle('update-config-data', (_event, ...args: Parameters<UpdateConfigData>) =>
+    updateConfigData(...args)
+  )
+
+  ipcMain.handle('read-song', (_event, ...args: Parameters<ReadSong>) => readSong(...args))
+  ipcMain.handle('new-playlist', (_event, ...args: Parameters<NewPlaylist>) => newPlaylist(...args))
+  ipcMain.handle('remove-playlist', (_event, ...args: Parameters<RemovePlaylist>) =>
+    removePlaylist(...args)
+  )
+  ipcMain.handle('rename-playlist', (_event, ...args: Parameters<RenamePlaylist>) =>
+    renamePlaylist(...args)
+  )
+
+  ipcMain.on('window-close', () => handleCloseWindow())
+  ipcMain.on('window-minimize', () => handleMinimizeWindow())
+  ipcMain.on('window-maximize', () => toggleMaximizeWindow())
+  ipcMain.on('open-song-folder', (_event, ...args: Parameters<OpenFolder>) =>
+    openSongFolder(...args)
+  )
+})
+
+// Create Window //
+
+app.whenReady().then(async () => {
   electronApp.setAppUserModelId('com.electron')
 
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
 
-  protocol.handle('local-resource', (request) => localResource(request))
-  ipcMain.on('window-close', () => handleCloseWindow())
-  ipcMain.on('window-minimize', () => handleMinimizeWindow())
-  ipcMain.on('open-song-folder', () => openSongFolder())
-  ipcMain.handle('song-list', () => getSongList())
-  ipcMain.handle('read-song', (_event, ...args: Parameters<ReadSong>) => readSong(...args))
-  ipcMain.handle('remove-song', (_event, ...args: Parameters<RemoveSong>) => removeSong(...args))
+  try {
+    await ensureDefaultPlaylistsFolders()
+    await ensureJsonConfig()
+
+    console.log('✅ Configuración inicializada')
+  } catch (err) {
+    console.error('❌ Error al inicializar la configuración:', err)
+  }
 
   createWindow()
-
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
 })
 
 app.on('window-all-closed', () => {
-  clearCacheMusicDir()
-
   if (process.platform !== 'darwin') {
     app.quit()
   }
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,14 +1,35 @@
+import {
+  ReadSong,
+  GetSongsPlayList,
+  OpenFolder,
+  NewPlaylist,
+  RemovePlaylist,
+  RenamePlaylist,
+  UpdateConfigData
+} from '../shared/types'
 import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
-import { RemoveSong, ReadSong } from '../shared/types'
 
 const api = {
-  getSongs: () => ipcRenderer.invoke('song-list'),
+  getPlayLists: () => ipcRenderer.invoke('play-list'),
+  getSongsPlaylist: (...args: Parameters<GetSongsPlayList>) =>
+    ipcRenderer.invoke('songs-playlist', ...args),
+  removePlaylist: (...args: Parameters<RemovePlaylist>) =>
+    ipcRenderer.invoke('remove-playlist', ...args),
+  renamePlaylist: (...args: Parameters<RenamePlaylist>) =>
+    ipcRenderer.invoke('rename-playlist', ...args),
+
+  getConfigData: () => ipcRenderer.invoke('get-config-data'),
+  updateConfigData: (...args: Parameters<UpdateConfigData>) =>
+    ipcRenderer.invoke('update-config-data', ...args),
+
+  newPlaylist: (...args: Parameters<NewPlaylist>) => ipcRenderer.invoke('new-playlist', ...args),
   readSong: (...args: Parameters<ReadSong>) => ipcRenderer.invoke('read-song', ...args),
-  removeSong: (...args: Parameters<RemoveSong>) => ipcRenderer.invoke('remove-song', ...args),
-  openSongFolder: () => ipcRenderer.send('open-song-folder'),
+
   windowClose: () => ipcRenderer.send('window-close'),
-  windowMinimize: () => ipcRenderer.send('window-minimize')
+  windowMinimize: () => ipcRenderer.send('window-minimize'),
+  toggleMaximize: () => ipcRenderer.send('window-maximize'),
+  openSongFolder: (...args: Parameters<OpenFolder>) => ipcRenderer.send('open-song-folder', ...args)
 }
 
 if (process.contextIsolated) {
