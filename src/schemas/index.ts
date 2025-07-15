4009221b8a6eac54a9f19915e0c2d9ddc8666828
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-15 10:22:16 -0500

    FEAT: add default playlist schema ⚙️

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,11 +2,14 @@ import * as z from 'zod'
 
 export const playlistName = z.object({
   playlistName: z
-    .string({
-      message: 'El nombre de la playlist es requerido'
-    })
-    .min(1, {
-      message: 'El nombre de la playlist es requerido'
-    })
+    .string({ error: 'El nombre de la playlist es requerido' })
+    .min(1, { error: 'El nombre de la playlist es requerido' })
 })
 export type PlaylistName = z.infer<typeof playlistName>
+
+export const defaultPlaylistFolder = z.object({
+  defaulFolder: z
+    .string({ error: 'Selecciona una playlist por defecto' })
+    .min(1, { error: 'Selecciona una playlist por defecto' })
+})
+export type DefaultPlaylistFolder = z.infer<typeof defaultPlaylistFolder>
