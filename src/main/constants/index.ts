e03b28c98cf89a1ecd4ad8fc1e2aa3502981f857
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-14 20:30:13 -0500

    CHORE: add config data constant ⚒️

--- src/main/constants/index.ts
+++ src/main/constants/index.ts
@@ -1,11 +1,21 @@
 import { homedir } from 'os'
 import { join } from 'path'
+import { UserConfig } from '../../shared/models'
+
+export const configData: UserConfig = {
+  defaultFolder: 'My Music',
+  theme: 'dark',
+  autoplay: false,
+  loop: false,
+  shuffle: false,
+  volume: 0.5
+}
 
 export const LOCAL_RESOURCE_PREFIX = 'local-resource://'
 export const EXTENSIONS = ['mp3', 'mp4']
 
 export const openfyMusicDir = join(homedir(), 'openfy-music')
 
-export const musicDir = join(openfyMusicDir, 'Music')
 export const cacheDir = join(openfyMusicDir, 'Cache')
 export const playListsDir = join(openfyMusicDir, 'Playlists')
+export const configDir = join(openfyMusicDir, 'Config')
