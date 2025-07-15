d9a17097048feeebf729548305579a5152f6e919
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-14 20:24:03 -0500

    CHORE: remove missing files ⚙️

--- src/main/helpers/convert-path.ts
+++
@@ -1,8 +0,0 @@
-export function convertPath(originalPath: string) {
-  const match = originalPath.match(/^\/([a-zA-Z])\/(.*)$/)
-  if (match) {
-    return `${match[1]}:/${match[2]}`
-  } else {
-    return originalPath
-  }
-}
