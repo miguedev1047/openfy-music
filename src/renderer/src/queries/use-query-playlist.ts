2047d8e64d0062a324091415b2f8846f3be43c72
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-15 17:22:05 -0500

    FEAT: add playlist folder fetch query ⚙️

---
+++ src/renderer/src/queries/use-query-playlist.ts
@@ -0,0 +1,15 @@
+import { queryOptions, useQuery } from '@tanstack/react-query'
+
+export const getPlaylists = async () => {
+  const data = await window.api.getPlayLists()
+  if (!data) throw new Error('An ocurred error on fetching data!')
+  return data
+}
+
+export const playlistsFoldersQueryOptions = queryOptions({
+  queryKey: ['playlists'],
+  queryFn: getPlaylists,
+  refetchInterval: 5000
+})
+
+export const usePlaylistFolders = () => useQuery(playlistsFoldersQueryOptions)
