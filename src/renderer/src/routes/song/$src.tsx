4b58301a8ab7aee97a6248fe25d3f084414c7b51
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-15 16:25:16 -0500

    CHORE: replace div with box in song route component 🎨

--- src/renderer/src/routes/song/$src.tsx
+++ src/renderer/src/routes/song/$src.tsx
@@ -1,4 +1,5 @@
 import { SongView } from '@renderer/components/song-view'
+import { Box } from '@renderer/components/ui/box'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/song/$src')({
@@ -7,8 +8,8 @@ export const Route = createFileRoute('/song/$src')({
 
 function RouteComponent() {
   return (
-    <div className="glass-item size-full grid place-items-center p-4 flex-1 flex-col [view-transition-name:main-content] relative">
+    <Box className="size-full grid place-items-center p-4 flex-1 flex-col [view-transition-name:main-content] relative overflow-hidden">
       <SongView />
-    </div>
+    </Box>
   )
 }
