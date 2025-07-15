2d2405c3ab479866d80a79e2f586717860d0f48a
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-14 20:32:23 -0500

    FEAT: add preload types ⚙️

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,16 +1,23 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
-import { SongProps, RemoveSongProps } from '../main/types/song.props'
+import { PlaylistFolderProps, RenamePlaylistFolderProps, SongProps, UserConfig } from '@shared/models'
 
 declare global {
   interface Window {
     electron: ElectronAPI
     api: {
       getSongs: () => Promise<SongProps[]>
+      getSongsPlaylist: (playlistName: string | undefined) => Promise<SongProps[]>
+      getConfigData: () => Promise<UserConfig>
+      getPlayLists: () => Promise<PlaylistFolderProps[]>
+      updateConfigData: (data: Partial<UserConfig>) => Promise<void>
+      newPlaylist: (playlistName: string) => Promise<void>
+      removePlaylist: (playlistName: string) => Promise<void>
+      renamePlaylist: (data: RenamePlaylistFolderProps) => Promise<void>
       readSong: (filename: string) => Promise<string>
-      removeSong: (data: RemoveSongProps) => Promise<void>
-      openSongFolder: () => Promise<void>
+      openSongFolder: (playlistName: string) => Promise<void>
       windowClose: () => Promise<void>
       windowMinimize: () => Promise<void>
+      toggleMaximize: () => Promise<void>
     }
   }
 }
