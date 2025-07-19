d362367a6910e9663e00690d9f3a7abc572f9b12
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-19 16:14:05 -0500

    CHORE: add find binary utility ⚙️

--- src/main/helpers/utils.ts
+++ src/main/helpers/utils.ts
@@ -1,4 +1,8 @@
-import { EXTENSIONS } from '../constants'
+import { join } from 'path'
+import { binDir, EXTENSIONS } from '../constants'
+import { existsSync } from 'fs'
+
+export const ffmpegPath = getBinaryPath(process.platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg')
 
 export function filterByExtension(song: string) {
   return EXTENSIONS.some((extension) => song.endsWith(extension))
@@ -10,4 +14,14 @@ export function jsonConverter(data: unknown): string {
 
 export function removeExtension(song: string) {
   return song.split('.')[0] || song
+}
+
+export function getBinaryPath(name: string) {
+  const binPath = join(binDir, name)
+
+  if (!existsSync(binPath)) {
+    throw new Error(`❌ No se encontró el binario requerido: ${name} en la carpeta bin.`)
+  }
+
+  return binPath
 }
