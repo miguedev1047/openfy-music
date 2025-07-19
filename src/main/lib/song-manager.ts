59a52ef17f31b517cd0efa4ee37a37f9f8233b08
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-19 12:20:38 -0500

    FEAT: add remove song function ⚙️

--- src/main/lib/song-manager.ts
+++ src/main/lib/song-manager.ts
@@ -1,5 +1,18 @@
-import { readFile } from 'fs/promises'
+import { readFile, rm } from 'fs/promises'
+import { join } from 'path'
+import { cacheDir, playListsDir } from '../constants'
+import { RemoveSongProps } from '../../shared/models'
 
 export async function readSong(filename: string): Promise<Buffer<ArrayBufferLike>> {
   return await readFile(filename)
+}
+
+export async function removeSong(props: RemoveSongProps) {
+  const { filename, playlist, id } = props
+
+  const fileSource = join(playListsDir, playlist, filename)
+  const cacheSource = join(cacheDir, `cover-${id}.png`)
+
+  await rm(fileSource)
+  await rm(cacheSource)
 }
