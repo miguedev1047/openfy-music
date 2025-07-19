7a1f1eee4f86139197fdaf56623ad1025d0ae5c4
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-19 16:13:41 -0500

    CHORE: add bin folder support and binary checks to main process ⚙️

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,7 +2,7 @@ import icon from '../../resources/icon.png?asset'
 
 import { app, shell, BrowserWindow, ipcMain, protocol } from 'electron'
 import { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import { electronApp, is, optimizer } from '@electron-toolkit/utils'
 
 import {
   GetSongsPlayList,
@@ -28,13 +28,15 @@ import {
   removePlaylist,
   renamePlaylist
 } from './lib/playlist-manager'
-import { openSongFolder } from './lib/open-song-folder'
+import { openBinFolder, openPlaylistFolder } from './lib/open-song-folder'
 import { readSong, removeSong } from './lib/song-manager'
+import { DownloadMusicMP3 } from './lib/download-music-mp3'
+import { checkBinaries } from './lib/check-binaries'
 
 import { localResource } from './helpers/local-resource'
-import { ensureDefaultPlaylistsFolders } from './utils/default-folder'
+import { ensureBinFolder, ensureDefaultPlaylistsFolders } from './utils/default-folder'
 import { ensureJsonConfig, getConfigData, updateConfigData } from './utils/config-data'
-import { DownloadMusicMP3 } from './lib/download-music-mp3'
+
 
 const gotTheLock = app.requestSingleInstanceLock()
 
@@ -144,13 +146,16 @@ app.whenReady().then(() => {
   ipcMain.on('window-close', () => handleCloseWindow())
   ipcMain.on('window-minimize', () => handleMinimizeWindow())
   ipcMain.on('window-maximize', () => toggleMaximizeWindow())
-  ipcMain.on('open-song-folder', (_event, ...args: Parameters<OpenFolder>) =>
-    openSongFolder(...args)
+  ipcMain.on('open-playlist-folder', (_event, ...args: Parameters<OpenFolder>) =>
+    openPlaylistFolder(...args)
   )
+  ipcMain.on('open-bin-folder', () => openBinFolder())
 
   ipcMain.handle('download-music-mp3', (_event, ...args: Parameters<DownloadMusic>) =>
     DownloadMusicMP3(...args)
   )
+
+  ipcMain.handle('check-binaries', () => checkBinaries())
 })
 
 // Create Window //
@@ -161,11 +166,13 @@ app.whenReady().then(async () => {
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
 
   try {
+    await ensureBinFolder()
     await ensureDefaultPlaylistsFolders()
     await ensureJsonConfig()
 
