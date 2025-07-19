d222b9b2f558757be71dd1a2a168b1f054d666b8
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-19 12:17:13 -0500

    FEAT: add background color for callback song image component 🎨

--- src/renderer/src/components/song-image/song-image.tsx
+++ src/renderer/src/components/song-image/song-image.tsx
@@ -27,36 +27,35 @@ function SimpleImage({ src, alt, className, loading = 'lazy' }: ImageProps) {
 
 function BlurredImage({ src, alt }: Omit<ImageProps, 'className' | 'loading'>) {
   return (
-    <div className='relative w-full h-full'>
+    <div className="relative w-full h-full">
+      <SimpleImage src={src} alt={alt} className="relative z-10" loading="lazy" />
       <SimpleImage
         src={src}
         alt={alt}
-        className='relative z-10'
-        loading='lazy'
-      />
-      <SimpleImage
-        src={src}
-        alt={alt}
-        className='absolute right-5 -bottom-[50%] -translate-y-[50%] scale-110 z-0 blur-3xl opacity-80'
-        loading='lazy'
+        className="absolute right-5 -bottom-[50%] -translate-y-[50%] scale-110 z-0 blur-3xl opacity-80"
+        loading="lazy"
       />
     </div>
   )
 }
 
 function ImagePlaceholder() {
-  return <Music className='size-[50%]' />
+  return (
+    <figure className="size-full bg-muted rounded-(--radius) grid place-items-center">
+      <Music className="size-[50%]" />
+    </figure>
+  )
 }
 
 function useImageRenderer(src: string | undefined | null, alt: string, isBlurred: boolean) {
   if (!src) {
     return <ImagePlaceholder />
   }
-  
+
   if (isBlurred) {
     return <BlurredImage src={src} alt={alt} />
   }
-  
+
   return <SimpleImage src={src} alt={alt} />
 }
 
@@ -64,10 +63,10 @@ export function SongImage({
   className,
   src,
   alt,
-  isBlurred = false,
+  isBlurred = false
 }: SongImageComponentProps): React.JSX.Element {
   const imageContent = useImageRenderer(src, alt, isBlurred)
-  
+
   return (
     <figure
       className={cn(
