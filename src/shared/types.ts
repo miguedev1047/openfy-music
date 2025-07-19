fbbfd70d85bad996d5cf5e0ea1a4f102a9b1c90f
miguel angel <miguelangeljimenez1047@gmail.com>
2025-07-19 12:03:11 -0500

    FEAT: add download type ⚒️

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,11 +1,20 @@
-import { PlaylistFolderProps, RenamePlaylistFolderProps, SongProps, UserConfig } from './models'
+import {
+  DownloadMusicProps,
+  PlaylistFolderProps,
+  RemoveSongProps,
+  RenamePlaylistFolderProps,
+  SongProps,
+  UserConfig
+} from './models'
 
 export type GetPlayLists = () => Promise<PlaylistFolderProps[]>
 export type GetSongsPlayList = (playlistName: string) => Promise<SongProps[]>
 export type GetConfigData = () => Promise<UserConfig>
 export type UpdateConfigData = (data: UserConfig) => Promise<void>
 export type RemovePlaylist = (playlistName: string) => Promise<void>
+export type RemoveSong = (data: RemoveSongProps) => Promise<void>
 export type RenamePlaylist = (data: RenamePlaylistFolderProps) => Promise<void>
 export type NewPlaylist = (playlistName: string) => Promise<void>
 export type ReadSong = (filename: string) => Promise<Buffer<ArrayBufferLike>>
 export type OpenFolder = (playlistName: string) => Promise<void>
+export type DownloadMusic = (data: DownloadMusicProps) => Promise<void>
